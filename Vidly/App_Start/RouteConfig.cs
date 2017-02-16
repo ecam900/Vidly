cd11dbf6c9fd9d897daa8bef265c66d515cacc6d
Eric Crescioni <ecam900@gmail.com>
2017-02-15 23:54:03 -0400

    Added parameters to route url

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -17,7 +17,8 @@ namespace Vidly
             routes.MapRoute(
                 "MoviesByReleaseDate",
                 "movies/released/{year}/{month}",
-                new { controller = "Movies", action = "ByReleaseDate" });
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"\d{4}", month = @"\d{2}" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",

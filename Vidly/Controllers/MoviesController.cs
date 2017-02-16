c8275c29749ac355078818636482eef659a0be46
Eric Crescioni <ecam900@gmail.com>
2017-02-15 23:44:43 -0400

    Add MoviesByReleaseDate route.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,11 +10,29 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ViewResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+              return View(movie);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
